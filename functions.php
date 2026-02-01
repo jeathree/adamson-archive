1afce472334136970f090ecb8bcfc6fa80f1c3f2
Jack Adamson <jadamson@gemcomedical.com>
2026-02-01 11:42:12 -0500

    Starting Point

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+    // Add Styles to Frontend
+    function child_enqueue_styles() {
+        wp_enqueue_style('child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100);
+    }
+    add_action('wp_enqueue_scripts', 'child_enqueue_styles', 999);
+
+    // Add Styles to Admin
+    add_action('enqueue_block_editor_assets', function() {
+        wp_enqueue_style(
+            'child-theme-editor-styles',
+            get_stylesheet_directory_uri() . '/style.css',
+            array(),
+            '1.0'
+        );
+    });
